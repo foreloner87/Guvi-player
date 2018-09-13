1f2902c48d16c40b5a159e227cd9b96817a4776b
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2018-09-13 18:15:18 +0530

    Update SIEVES_bwrange.cpp

--- SIEVES_bwrange.cpp
+++ SIEVES_bwrange.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 void primecheck(long n1,long n2)
 {
-	bool c[1001];
+	bool c[100001];
 	memset(c,true,sizeof(c));
-	for(int i=2;i<=1000;i++)
+	for(long i=2;i<=100000;i++)
 	if(c[i]==true)
-	for(int j=i*2;j<=1000;j+=i)
+	for(long j=i*2;j<=100000;j+=i)
 	c[j]=false;
 	c[0]=c[1]=false;
-	for(int i=n1+1;i<n2;i++)
+	for(long i=n1+1;i<n2;i++)
 	if(c[i]==true)
 	cout<<i<<' ';
 }
